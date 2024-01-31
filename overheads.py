3c7922135f1ca47dcd605beb1aee066a6e1f2b93
haydeengg <S10241547@connect.np.edu.sg>
2024-01-31 14:56:08 +0800

    did overheads

--- overheads.py
+++ overheads.py
@@ -14,3 +14,16 @@ with fp.open(mode="r", encoding="UTF-8", newline="") as file:
         #get the net profit
         #and append to the profit and loss
         overhead_records.append([row[0], row[1]])  
+
+
+def topoverhead(): #finds the highest overhead
+    overhead_values = [] #list used to store values
+    overhead_dict = {} #dictionary to store categories as values and overheads as keys
+    for record in overhead_records: #adds values to both and keys to dict
+        overhead_values.append(float(record[1]))
+        overhead_dict[record[1]] = record[0]
+    overhead_values.sort(reverse=True) #to find the highest overhead
+    # use the dictionary to find the category
+    return f"[HIGHEST OVERHEAD] {overhead_dict[str(overhead_values[0])].upper()}: {overhead_values[0]}%"
+
+print(topoverhead())
